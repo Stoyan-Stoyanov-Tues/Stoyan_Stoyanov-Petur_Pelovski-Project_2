a4133ce8458ebfb58390579ba004880e66f4b9e0
Petar <peturpelovski@gmail.com>
2014-06-18 11:58:26 +0300

    Create first.rb

---
+++ project2/first_part/first.rb
@@ -0,0 +1,11 @@
+require 'nokogiri'
+
+builder = Nokogiri::HTML::Builder.new do |doc|
+	doc.html {
+		doc.body(:onload => 'some_func();') {
+			doc.span.bold {
+				doc.text "Hello world"
+			}
+		}
+	}
+end
